bab79b81f58c9c0e1aea3e0dd95081e60acf47a4
Artavazd Barseghyan <artavazd1@gmail.com>
2025-06-11 00:32:55 +0200

    first commit

---
+++ src/types/auth.dto.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+export interface AuthenticateDTO {
+  identity: string;
+  password: string;
+}
+
+export interface CreateTokenDTO {
+  identity: string;
+  password?: string;
+  code?: string;
+}
+
+export const CreateTokenDTOSchema = z.object({
+  client: z.string(),
+  password: z.string().optional(),
+  code: z.string().optional(),
+});
+
+export interface DidCreateTokenDTO {
+  access_token: string;
+  refresh_token: string;
+}
+
+export interface RefreshTokenDTO {
+  refresh_token: string;
+}
+
+export const RefreshTokenDTOSchema = z.object({
+  refresh_token: z.string(),
+});
+
+export interface DidRefreshTokenDTO {
+  access_token: string;
+  refresh_token: string;
+}
