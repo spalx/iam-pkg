001311ff87e2f93e037199f6475c2e030663e63f
Artavazd Barseghyan <artavazd1@gmail.com>
2025-08-27 15:45:35 +0200

    update

--- src/types/user.dto.ts
+++ src/types/user.dto.ts
@@ -1,11 +1,11 @@
-export interface UserRoleEntityDTO {
-  id: number;
+export interface RoleEntityDTO {
+  id: string;
   name: string;
   permissions: string[];
 }
 
 export interface UserEntityDTO {
-  id: number;
+  id: string;
   identity: string;
   user_roles: UserRoleEntityDTO[];
   meta: Record<string, unknown>;
