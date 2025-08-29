a2cfdd4b5b9e91145391150271c13bc1995279e7
Artavazd Barseghyan <artavazd1@gmail.com>
2025-08-29 19:51:08 +0200

    user action added

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,14 @@
-export enum IAMAction {
-  Authenticate = 'iam.authenticate',
-  CreateToken = 'iam.createToken',
-  RefreshToken = 'iam.refreshToken',
-  RevokeToken = 'iam.revokeToken',
+export enum AuthAction {
+  Authenticate = 'iam.auth.authenticate',
+  CreateToken = 'iam.auth.createToken',
+  RefreshToken = 'iam.auth.refreshToken',
+  RevokeToken = 'iam.auth.revokeToken',
+}
+
+export enum UserAction {
+  GetUsers = 'iam.user.getUsers',
+  GetUser = 'iam.user.getUser',
+  CreateUser = 'iam.user.createUser',
+  UpdateUser = 'iam.user.updateUser',
+  DeleteUser = 'iam.user.deleteUser',
 }
